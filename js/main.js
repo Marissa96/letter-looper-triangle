d4164c5c7dcb3c26bec1cbec032850328326c6a4
Marissa96 <marissa.boswall96@gmail.com>
2016-02-26 17:48:10 -0500

    Done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var letter = prompt('Enter a letter:');
+var counter;
+var counter2;
+
+for (counter = 0; counter < 10; counter++) {
+  for (counter2 = 0; counter2 <= counter; counter2++) {
+  document.write(letter);
+}
+  document.write(' <br> ');
+}
